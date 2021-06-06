3904a0a581e656f927ba657f8ed204fdf30d3823
UnknownNpc <artem2001sarg@gmail.com>
2021-06-07 00:20:42 +0300

    new favicon. reset report data - new message

--- src/pages/CreateReport.vue
+++ src/pages/CreateReport.vue
@@ -1,40 +1,54 @@
 <template>
-<q-page class="q-pa-md">
-  <report-stepper/>
-</q-page>
+  <q-page class="q-pa-md">
+    <report-stepper/>
+  </q-page>
 </template>
 
 <script lang="ts">
   import ReportStepper from 'src/components/report/create_report/report_stepper/ReportStepper.vue';
-  import { useQuasar } from 'quasar'
+  import { useQuasar } from 'quasar';
   import reportUtils from 'src/utils/report/ReportUtils';
-  import {defineComponent} from 'vue'
-  import {useStore} from 'src/store'
+  import { defineComponent } from 'vue';
+  import { useStore } from 'src/store';
   import notifyApi from 'src/api/NotifyApi';
+  import { IReport } from 'src/model/report/Report';
 
   export default defineComponent({
     name: 'CreateReportPage',
-    components:{ReportStepper},
-    setup(){
-      const q = useQuasar()
-      const store = useStore()
+    components: { ReportStepper },
+    setup() {
+      const q = useQuasar();
+      const store = useStore();
+
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      function showDialog():void{
+      function showDialog(): void {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-        if(store.getters['report/getReport']){
-          q.dialog({
-            message:'Хотите сбросить данные',
-            cancel:true,
-            ok:true
-          }).onOk(()=>{
-            reportUtils.setDefaultReportTablesData()
-            notifyApi.showPositive('Форма отчета успешно сброшена')
-          })
+        let report: IReport = store.getters['report/getReport'] as IReport;
+        if (report) {
+          if (report.id) {
+            q.dialog({
+              message: `Хотите сбросить данные. Вы редактируетете существуюущий отчет: ${report?.reportName || 'Unnamed'}`,
+              cancel: true,
+              ok: true
+            }).onOk(() => {
+              reportUtils.setDefaultReportTablesData();
+              notifyApi.showPositive('Форма отчета успешно сброшена');
+            });
+          } else {
+            q.dialog({
+              message: 'Хотите сбросить данные',
+              cancel: true,
+              ok: true
+            }).onOk(() => {
+              reportUtils.setDefaultReportTablesData();
+              notifyApi.showPositive('Форма отчета успешно сброшена');
+            });
+          }
         }
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       }
-      showDialog()
-      return {}
+
+      showDialog();
+      return {};
     }
   });
 </script>
